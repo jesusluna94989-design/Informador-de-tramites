b3a92928bc56452577f56b6c8ec95677783b15f2
Jesus Luna <jesusluna94989@gmail.com>
2025-12-02 00:13:51 -0700

    Add files via upload

---
+++ ProyectoAdministracion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoadministracion;
+
+/**
+ *
+ * @author jesus
+ */
+public class ProyectoAdministracion {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new InformadorDeTramites().setVisible(true);
+            }
+        });
+    }
+}
